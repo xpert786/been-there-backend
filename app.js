689191d944778659a863132bcbb1a5165e9d98f9
Arun Rana <m.xpert786@gmail.com>
2025-12-03 14:26:11 +0530

    added cokkies

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocs = require('./swagger/swaggerOptions');
+const cookieParser = require("cookie-parser");
 require('dotenv').config();
 
 // Validate required environment variables
@@ -37,7 +38,7 @@ app.use(
     credentials: true,
   })
 );
-
+app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
